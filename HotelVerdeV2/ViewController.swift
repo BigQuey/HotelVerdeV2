a2404adf04b181d81e9cdf68f648664c4c7e596a
Queyvin <queyvinrl@gmail.com>
2025-12-20 13:24:55 -0500

    first commit

---
+++ HotelVerdeV2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HotelVerdeV2
+//
+//  Created by DAMII on 20/12/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
